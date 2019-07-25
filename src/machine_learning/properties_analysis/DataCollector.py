16b4386156d482955107295d3e95742e073eae63
YannisSchmutz <yannis.schmutz@gmail.com>
2019-07-25 19:46:53 +0200

    minorchange: Use better formatted logs in DataCollector (master)

--- src/machine_learning/properties_analysis/DataCollector.py
+++ src/machine_learning/properties_analysis/DataCollector.py
@@ -16,21 +16,21 @@ CITIES = ['bern', 'zuerich', 'basel']
 response_string = ""
 
 for city in CITIES:
-    log.info("Collecting data from {city}".format(city=city))
+    log.info(f"Collecting data from {city}")
     # Collecting 25 pages per city should be enough for now.
     # One page contains up to 24 articles
     for page_nbr in range(1, 25):
         url = BASE_URL + LOCATION_URL.format(city=city) + PAGE_N.format(nbr=page_nbr)
-        log.info("Using URL: {url}".format(url=url))
+        log.info(f"Using URL: {url}")
         resp = requests.get(url)
         if resp.status_code == 200:
             log.success("Request succeeded")
             response_string += resp.text
         else:
             # Exceeded the maximum amount of pages
-            log.fail("Request failed, continue with next city")
+            log.fail("Request failed, continue with next city...")
             break
         sleep(2)  # Don't spam them too much
 
-with open("immo_data.raw", "w") as f:
+with open("properties_html_data.raw", "w") as f:
     f.write(response_string)
