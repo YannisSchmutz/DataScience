605603c4810df424e6f31eabeaa1cae612985377
YannisSchmutz <yannis.schmutz@gmail.com>
2019-07-28 18:03:35 +0200

    feat: Add pragmatic logger functions

---
+++ src/miscellaneous/pseudoLogger.py
@@ -0,0 +1,12 @@
+
+
+def info(msg):
+    print("[*] {msg}".format(msg=msg))
+
+
+def fail(msg):
+    print("[-] {msg}".format(msg=msg))
+
+
+def success(msg):
+    print("[+] {msg}".format(msg=msg))
