0318cadab7e3464c14cec9261713230206fb99e3
YannisSchmutz <yannis.schmutz@gmail.com>
2019-08-25 22:44:45 +0200

    fix: Correct plot label

--- src/data-visualization/NormalDistribution.py
+++ src/data-visualization/NormalDistribution.py
@@ -9,14 +9,14 @@ fig, ((sub1, sub2), (sub3, sub4)) = plt.subplots(2, 2)
 xs = [x / 10.0 for x in range(-50, 50)]
 
 sub1.plot(xs, [pdf(x) for x in xs], '-', label="mu=0, sigma=1")
-sub1.plot(xs, [pdf(x, 0, 2) for x in xs], '.', label="mu=0, sigma=1")
+sub1.plot(xs, [pdf(x, 0, 2) for x in xs], '.', label="mu=0, sigma=2")
 sub1.plot(xs, [pdf(x, 0, 0.5) for x in xs], ':', label="mu=0, sigma=0.5")
 sub1.plot(xs, [pdf(x, -1, 1) for x in xs], '-.', label="mu=-1, sigma=1")
 sub1.legend()
 sub1.set_title("Some normal distribution probability density functions")
 
 sub3.plot(xs, [cdf(x) for x in xs], '-', label="mu=0, sigma=1")
-sub3.plot(xs, [cdf(x, 0, 2) for x in xs], '.', label="mu=0, sigma=1")
+sub3.plot(xs, [cdf(x, 0, 2) for x in xs], '.', label="mu=0, sigma=2")
 sub3.plot(xs, [cdf(x, 0, 0.5) for x in xs], ':', label="mu=0, sigma=0.5")
 sub3.plot(xs, [cdf(x, -1, 1) for x in xs], '-.', label="mu=-1, sigma=1")
 sub3.legend()
